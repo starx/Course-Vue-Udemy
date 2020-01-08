1ecec727b127c771788923ea1cfbd54f6eac0a43
starx <swe_r3p0x9@mrnepal.com>
2020-01-08 07:26:07 +0000

    [WIP] Further progress on monster game

--- Assignments/dist/app.js
+++ Assignments/dist/app.js
@@ -1,27 +1,67 @@
-// import Vue from 'vue';
-// import Vuex from 'vuex';
-//
-// Vue.use(Vuex);
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        count: 0 ,
-        products: [
-            { id: 1, title: 'Apple', category: 'fruit' },
-            { id: 2, title: 'Orange', category: 'fruit' },
-            { id: 3, title: 'Carrot', category: 'vegetable' }
-        ]
+        playerHealth: 80,
+        monsterHealth: 90,
+        gameIsRunning: false
     },
     mutations: {
-        increment (state) { state.count++ }
+        startGame(state) {
+            state.gameIsRunning = true;
+        },
+        decreasePlayerHealth(state, payload) {
+            console.log(payload);
+            state.playerHealth -= payload.value;
+        },
+        decreaseMonsterHealth(state, payload) {
+            console.log(payload);
+            state.monsterHealth -= payload.value;
+        },
+    },
+    actions: {
+        getRandomAttackValue({ state }) {
+            console.log('random called')
+            let max = 10;
+            let min = 1;
+            let randomAttack = Math.floor(Math.random() * (max - min + 1) + min);
+            let attack = randomAttack;
+            return attack;
+        },
+        attackMonster({dispatch, commit }) {
+            dispatch('getRandomAttackValue').then((attackValue) => {
+                commit('decreaseMonsterHealth', { value: attackValue });
+            });
+        },
+        attackPlayer({dispatch, commit }) {
+            dispatch('getRandomAttackValue').then((attackValue) => {
+                commit('decreasePlayerHealth', { value: attackValue });
+            });
+        },
     }
-})
+});
+var app = new Vue({
+    el: "#app",
+    store,
+    data() {
+        return {}
+    },
+    computed: {
 
+        ...Vuex.mapState([
+            'playerHealth',
+            'monsterHealth',
+            'gameIsRunning',
+        ]),
+    },
+    methods: {
+        ...Vuex.mapMutations([
+            'startGame'
+        ]),
+        ...Vuex.mapActions([
+            'attackPlayer',
+            'attackMonster',
+        ])
 
-
-// Testing
-mocha.setup('bdd');
-let assert = chai.assert;
-let expect = chai.expect;
-
-mocha.run();
+    },
+});
