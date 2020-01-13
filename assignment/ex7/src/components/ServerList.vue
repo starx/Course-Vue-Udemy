3f7dc7eb298506832437bdff012ff39b820aaf36
starx <mail@mrnepal.com>
2020-01-13 16:08:55 +0000

    Added ex7

---
+++ assignment/ex7/src/components/ServerList.vue
@@ -0,0 +1,17 @@
+<template>
+  <li
+    class="list-group-item"
+    @click="$emit('serverSelected', id)"
+  >
+    Server #{{ id }}: {{ status }}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    status: String,
+  },
+};
+</script>
