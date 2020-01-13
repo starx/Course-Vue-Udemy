782493f270283c8ef99a4701f6a144926a104ba7
starx <swe_r3p0x9@mrnepal.com>
2020-01-13 16:08:55 +0000

    Added ex7

---
+++ assignment/ex7/src/components/Servers.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="list-group">
+    <ServerList
+      v-for="server in servers"
+      :key="server.id"
+      :id="server.id"
+      :status="server.status"
+      @serverSelected="$emit('serverSelected', $event)"
+    ></ServerList>
+  </ul>
+</template>
+
+<script>
+import ServerList from '@/components/ServerList.vue';
+
+export default {
+  props: {
+    servers: Array,
+  },
+  components: {
+    ServerList,
+  },
+};
+</script>
