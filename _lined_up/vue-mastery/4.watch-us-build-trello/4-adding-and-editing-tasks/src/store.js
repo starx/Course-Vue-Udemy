d49c45f0b6d004ad3f35231845e448cc900db60c
starx <mail@mrnepal.com>
2020-01-22 07:40:43 +0000

    Moved projects and added lined up course code from vue mastery

---
+++ _lined_up/vue-mastery/4.watch-us-build-trello/4-adding-and-editing-tasks/src/store.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import defaultBoard from './default-board'
+import { saveStatePlugin, uuid } from './utils'
+
+Vue.use(Vuex)
+
+const board = JSON.parse(localStorage.getItem('board')) || defaultBoard
+
+export default new Vuex.Store({
+  plugins: [saveStatePlugin],
+  state: {
+    board
+  },
+  getters: {
+    getTask (state) {
+      return (id) => {
+        for (const column of state.board.columns) {
+          for (const task of column.tasks) {
+            if (task.id === id) {
+              return task
+            }
+          }
+        }
+      }
+    }
+  },
+  mutations: {
+    CREATE_TASK (state, { tasks, name }) {
+      tasks.push({
+        name,
+        id: uuid(),
+        description: ''
+      })
+    },
+    UPDATE_TASK (state, { task, key, value }) {
+      task[key] = value
+      // Vue.set(task, key, value)
+    }
+  }
+})
---
+++ _lined_up/vue-mastery/7.animating-vue/2-transitions-end/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="toggleModal">Open</button>
+
+    <transition name="fade">
+      <div v-if="isOpen" class="modal">
+        <p>
+          <button @click="toggleModal">Close</button>
+        </p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
+
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
