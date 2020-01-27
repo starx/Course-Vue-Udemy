f0e60d5128cc2719cfbb8e1c47f5541511eab15f
starx <mail@mrnepal.com>
2020-01-27 05:36:21 +0000

    [WIP] adding pwa

--- 1.udemy-vue/project/project-3-stock-trader/src/main.js
+++ 1.udemy-vue/project/project-3-stock-trader/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import './registerServiceWorker';
+import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
@@ -8,6 +8,7 @@ import 'bootstrap';
 
 import './scss/vendor.scss';
 import './scss/main.scss';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 Vue.filter('currency', value => `$${value.toLocaleString()}`);
@@ -15,8 +16,11 @@ Vue.filter('currency', value => `$${value.toLocaleString()}`);
 new Vue({
   router,
   store,
+
   created() {
     this.$store.dispatch('loadAppData');
   },
+
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
--- 1.udemy-vue/project/project-3-stock-trader/vue.config.js
+++ 1.udemy-vue/project/project-3-stock-trader/vue.config.js
@@ -7,93 +7,14 @@ module.exports = {
     disableHostCheck: true,
     // public: 'js.test',
   },
-
-  // Below not working
-
-  // // The object will be merged into the final webpack config using webpack-merge.
-  // configureWebpack: {
-  //   plugins: [
-  //     new webpack.ProvidePlugin({
-  //       $: "jquery",
-  //       jQuery: "jquery",
-  //       "window.jQuery": "jquery"
-  //     })
-  //   ]
-  // },
-
-  // Not working end
-
-
-  // // If you need conditional behavior based on the environment, or want to directly mutate the
-  // // config, use a function (which will be lazy evaluated after the env variables are set). The
-  // // function receives the resolved config as the argument. Inside the function, you can either
-  // // mutate the config directly, OR return an object which will be merged
-  // configureWebpack: config => {
-  //   if (process.env.NODE_ENV === 'production') {
-  //     // mutate config for production...
-  //   } else {
-  //     // mutate for development...
-  //   }
-  // },
-  //
-
-
-  ////////////////
-  //
-  // OR
-  // The internal webpack config is maintained using webpack-chain.
-  // The library provides an abstraction over the raw webpack config,
-  // with the ability to define named loader rules and named plugins,
-  // and later "tap" into those rules and modify their options.
-  chainWebpack: config => {
-    // # Add a plugin
-    config
-      .plugin('provide')
-      .use(webpack.ProvidePlugin, [{
-        $: 'jquery',
-        jquery: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      }]);
-
-    // // # Modifying Options of a Loader
-    // config.module
-    //   .rule('vue')
-    //   .use('vue-loader')
-    //   .loader('vue-loader')
-    //   .tap(options => {
-    //     // modify the options...
-    //     return options
-    //   });
-    //
-    // // GraphQL Loader
-    // config.module
-    //   .rule('graphql')
-    //   .test(/\.graphql$/)
-    //   .use('graphql-tag/loader')
-    //   .loader('graphql-tag/loader')
-    //   .end();
-    //
-    // // # Replacing loaders of a rule
-    // const svgRule = config.module.rule('svg');
-    //
-    // // clear all existing loaders.
-    // // if you don't do this, the loader below will be appended to
-    // // existing loaders of the rule.
-    // svgRule.uses.clear();
-    //
-    // // add replacement loader(s)
-    // svgRule
-    //   .use('vue-svg-loader')
-    //   .loader('vue-svg-loader');
-    //
-    // // # Modifying Options of a Plugin
-    // config
-    //   .plugin('html')
-    //   .tap(args => {
-    //     // args[0].template = '/Users/username/proj/app/templates/index.html'
-    //     // return args
-    //     return [/* new args to pass to html-webpack-plugin's constructor */]
-    //   });
+  pwa: {
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: "src/sw.js"
+    }
   },
-}
+  transpileDependencies: [
+    'vuetify',
+  ],
+};
